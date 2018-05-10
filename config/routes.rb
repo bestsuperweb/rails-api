f7b7ab17e91c963184f6f1a6e9ba341e5bdb816f
bestsuperweb <bestsuperweb@gmail.com>
2018-05-10 08:44:14 -0400

    init rails api app

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+	scope module: :v2, constraints: ApiVersion.new('v2') do
+		resources :posts, only: :index
+	end
+	
+	scope module: :v1, constraints: ApiVersion.new('v1', true) do
+		resources :dislikes
+		resources :likes
+		resources :comments
+		resources :posts
+	end
+
+	resources :users
+	post 'auth/login', to: 'authentication#authenticate'
+
+end
