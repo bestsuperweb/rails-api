6909f40b1163d073a6e888a5566827091cfe1164
bestsuperweb <bestsuperweb@gmail.com>
2018-05-10 08:52:06 -0400

    api serialize

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,7 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :id, :body, :created_at, :updated_at
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+  has_many :dislikes
+end
