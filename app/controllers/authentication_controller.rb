4fd90594e976413d935e2a67ea77328891228f59
bestsuperweb <bestsuperweb@gmail.com>
2018-05-23 08:16:55 -0400

    implement login with twitter

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,9 +1,16 @@
 class AuthenticationController < ApplicationController  
-  skip_before_action :authorize_request, only: :authenticate
+  skip_before_action :authorize_request, only: [:authenticate, :authenticate_twitter]
   # return auth token once user is authenticated
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  def authenticate_twitter
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    p "------- auth_hash = #{auth_hash}"
+    auth_token = JsonWebToken.encode(user_id: @user.id)
     json_response(auth_token: auth_token)
   end
 
@@ -12,4 +19,11 @@ class AuthenticationController < ApplicationController
   def auth_params
     params.permit(:email, :password)
   end
+
+  protected
+ 
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
 
 	resources :users
 	post 'auth/login', to: 'authentication#authenticate'
+	get '/auth/:provider/callback', to: 'authentication#authenticate_twitter'
 
 end
