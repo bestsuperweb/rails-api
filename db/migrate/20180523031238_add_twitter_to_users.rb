9591df796d956c325633ac8f28bb1a73bd18d48c
bestsuperweb <bestsuperweb@gmail.com>
2018-05-23 08:15:50 -0400

    add new field into user

---
+++ db/migrate/20180523031238_add_twitter_to_users.rb
@@ -0,0 +1,7 @@
+class AddTwitterToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :auth_secret, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_03_182610) do
+ActiveRecord::Schema.define(version: 2018_05_23_031238) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 2018_05_03_182610) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "auth_secret"
   end
 
 end
