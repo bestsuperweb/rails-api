ed5fe6682e76f0ea55aa803df541b5f75a1f2940
bestsuperweb <bestsuperweb@gmail.com>
2018-05-23 08:16:03 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   has_many :dislikes
   validates_presence_of :name, :email, :password_digest
 
+  def self.find_or_create_from_auth_hash(auth_hash)
+    user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
+    user.update(
+      name: auth_hash.info.nickname,
+      auth_token: auth_hash.credentials.token,
+      auth_secret: auth_hash.credentials.secret
+    )
+    user
+  end
+
 end
