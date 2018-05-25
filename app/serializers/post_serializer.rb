3fb21ede34871261ecafb6c6dcc73d5b6f591c15
bestsuperweb <bestsuperweb@gmail.com>
2018-05-24 22:37:09 -0400

    comment out user in serialized post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :body, :created_at, :updated_at
-  belongs_to :user
+  # belongs_to :user
   has_many :comments
   has_many :likes
   has_many :dislikes
